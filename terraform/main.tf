c118354d1e4d11db417ed0224303ef3b3952ec31
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-01 20:55:58 +0000

    Allow a user set a secret in the key-vault created

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,4 +39,16 @@ resource "azurerm_key_vault" "main-key-vault" {
       "Delete"
     ]
   }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete"
+    ]
+  }
+
 }
