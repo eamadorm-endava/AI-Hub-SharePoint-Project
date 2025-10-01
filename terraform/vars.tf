ac0569f64e46275e66cf803f44d5e05cc80a5f8c
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-01 20:31:16 +0000

    Creation of an agent_identity and a key-vault in azure using Terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,6 +4,24 @@ variable "azure_subscription_id" {
   default     = "15158422-ef98-425f-a0e3-6e53c81ba478"
 }
 
+variable "tag_owner" {
+  type        = string
+  description = "Value of the tag 'Owner'"
+  default     = "Emmanuel_Amador"
+}
+
+variable "tag_tower" {
+  type        = string
+  description = "Value of the tag 'Tower'"
+  default     = "Infrastructure"
+}
+
+variable "tag_project" {
+  type        = string
+  description = "Value of the tag 'project'"
+  default     = "ai-hub-sharepoint"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Name of the main resource group. Manually created"
