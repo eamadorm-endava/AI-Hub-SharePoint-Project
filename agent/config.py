64e902c3f9241a805d95fefbae5a57f74346518a
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-02 04:56:50 +0000

    Add more parameters required by the AzureConfig and the AgentConfig classes

--- agent/config.py
+++ agent/config.py
@@ -1,14 +1,25 @@
 from pydantic_settings import BaseSettings
-from pydantic import Field
+from pydantic import Field, SecretStr
 from typing import Annotated
 
 
-class AgentConfig(BaseSettings):
+class AgentConfig(BaseSettings, validate_assignment=True):
+    """
+    Class that holds configuration values for the agent, it requires to assign
+    parameters after initialization.
+    """
+
     PROMPT_TEMPLATE_PATH: str = "prompts/page_generator.txt"
     OPENAI_SECRET_NAME: str = "OPENAI-API-KEY"
+    OPENAI_KEY: SecretStr = ""
 
 
 class AzureConfig(BaseSettings):
+    """
+    Class that holds configuration values for Azure services. Allowing to, in any future, change the
+    cloud provider or the way to access the secrets.
+    """
+
     KEY_VAULT_URL: Annotated[
         str,
         Field(
