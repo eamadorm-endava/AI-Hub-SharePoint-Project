3ba4f63294f6ccdb042c16a24f5e14d010073edb
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-04 00:16:22 -0600

    Add more steps to the ai_news_pipeline, now fetches news, filters by date, and stores the news into an excel file

--- ai_news_pipeline/ai_news_pipeline.py
+++ ai_news_pipeline/ai_news_pipeline.py
@@ -1,50 +1,28 @@
-import feedparser
 from loguru import logger
 from ai_news_pipeline.config import AINewsSettings
-from ai_news_pipeline.news_auxiliars import format_date, extract_news_image
+from ai_news_pipeline.ai_news_pipeline_steps import retrieve_ai_news, filter_news_by_date, store_to_excel
 
 
 news_settings = AINewsSettings()
 
 
-def retrieve_ai_news(url: str) -> list[dict]:
-    """
-    Retrieve AI news from the given RSS feed URL.
-
-    Args:
-        url (str): URL of the RSS feed.
-
-    Returns:
-        list[dict]: List of dictionaries containing news details.
-    """
-    if not isinstance(url, str):
-        raise ValueError("Input must be a string representing the RSS feed URL.")
-    feed = feedparser.parse(url)
-
-    # Extract relevant data from feed entries
-    ai_news = [
-        {
-            "title": entry.title,
-            "news_link": entry.link,
-            "publish_date": format_date(entry.published),
-            "image_link": extract_news_image(entry.link),
-            "summary": entry.summary,
-        }
-        for entry in feed.entries
-    ]
-
-    return ai_news
-
-
 def main():
     logger.info("Starting AI news retrieval process...")
-
+    
+    # Step 1: Retrieve AI news from RSS feed
     logger.info("Fetching AI news from RSS feed...")
-    ai_news = retrieve_ai_news(news_settings.NEWS_URL)
+    ai_news = retrieve_ai_news(news_settings.NEWS_URL, search_keywords=news_settings.SEARCH_KEYWORDS)
     logger.info(f"Retrieved {len(ai_news)} news articles:")
-    logger.info(
-        f"News Titles: \n{'\n'.join([article.get('title') for article in ai_news])}"
-    )
+
+    # Step 2: Filter news articles by date (last 2 days)
+    logger.info(f"Filtering news articles from the last {news_settings.DAYS_BACK} days...")
+    ai_news = filter_news_by_date(df=ai_news, date_column=news_settings.DATE_COLUMN, days=news_settings.DAYS_BACK)
+    logger.info(f"{len(ai_news)} articles remain after date filtering.")
+
+    # Step 3: Store the filtered news articles to an Excel file
+    logger.info("Storing AI news into an Excel file...")
+    store_to_excel(ai_news, news_settings.LOCAL_FILE_PATH)
+    logger.info(f"AI news successfully stored in {news_settings.LOCAL_FILE_PATH}")
 
 
 if __name__ == "__main__":
