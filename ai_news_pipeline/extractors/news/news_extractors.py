93d6daf0ca0dd1d53b806686642eb91186d3950f
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-08 01:03:09 -0600

    First implementation of the method _filter_by_keywords in the NewsExtractor class

--- ai_news_pipeline/extractors/news/news_extractors.py
+++ ai_news_pipeline/extractors/news/news_extractors.py
@@ -1,6 +1,7 @@
 import feedparser
 import pandas as pd
-from typing import Type
+from loguru import logger
+from typing import Type, Optional
 
 from ai_news_pipeline.config import AINewsConfig
 from ai_news_pipeline.extractors.image_url.extractor_selector import (
@@ -43,8 +44,8 @@ class NewsExtractor:
         Retrieves AI-related news data in its raw format from the feed_url. The data obtained per news is:
             - title
             - news_link
+            - image_link
             - publish_date
-        The news retrieved are filtered based in case_sen_search_kw and case_insen_search_kw
 
         Returns:
             list[dict] -> Each dictionary represents a news retrieved.
@@ -65,8 +66,66 @@ class NewsExtractor:
 
         return news
 
-    def _filter_by_keywords(self) -> list[dict]:
-        pass
+    def _filter_by_keywords(
+        self,
+        news_extracted: list[dict],
+        filter_key: str = "title",
+        case_sen_search_kw: list[str] = None,
+        case_insen_search_kw: list[str] = None,
+    ) -> Optional[list[dict]]:
+        """
+        News retrieved from _get_raw_parsed_data are filtered based in
+        case sensitive and case insensitive keywords
+
+        Args:
+            news_extracted: list[dict] -> List of dictionaries where each represents an
+                                          article. It must contain at least the
+                                          key set in 'filter_key' parameter
+            filter_key: str -> Name of the key to filter by. Default: 'title'
+            case_sen_search_kw: list[str] -> List of Case Sensitive Search Keywords.
+                                            Default: self.case_sen_search_kw
+            case_insen_search_kw: list[str] -> List of (Case Insensitive Search Keywords)
+                                            Default: self.case_insen_search_kw
+
+        Returns:
+            list[dict] -> List of dictionaries filtered
+        """
+        if not case_sen_search_kw:
+            case_sen_search_kw = self.case_sen_search_kw
+
+        if not case_insen_search_kw:
+            case_insen_search_kw = self.case_insen_search_kw
+
+        if not isinstance(news_extracted, list):
+            logger.error(
+                "The parameter news_extracted must be a list of dictionaries. "
+                "Each dictionary must, at least, contain the key set in filter_key "
+                "(default to 'title')"
+            )
+            return None
+
+        if not all(isinstance(article, dict) for article in news_extracted):
+            logger.error("Each entry of the news_extracted list must be dictionaries")
+            return None
+
+        if not all(filter_key in article.keys() for article in news_extracted):
+            logger.error(
+                "Each dictionary of the news_extracted list must contain the "
+                f"filter key: {filter_key}"
+            )
+            return None
+
+        news_filtered = [
+            article
+            for article in news_extracted
+            if any(kw in article.get(filter_key) for kw in case_sen_search_kw)
+            or any(
+                kw.lower() in article.get(filter_key).lower()
+                for kw in case_insen_search_kw
+            )
+        ]
+
+        return news_filtered
 
     def _filter_by_age(self) -> pd.DataFrame:
         pass
