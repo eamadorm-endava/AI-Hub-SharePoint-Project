eff3c37c4f4bbbbf34beec2f92b57d51b4f60cd5
Emmanuel Amador <emmanuel.amador@endava.com>
2025-10-09 17:36:21 -0600

    Deleting fields that are arguments of the pipeline instead of config values from the news_extraction pipeline

--- news_extraction_pipeline/config.py
+++ news_extraction_pipeline/config.py
@@ -3,48 +3,6 @@ from pydantic import Field
 from typing import Annotated
 
 
-# Common fields
-case_insen_search_kw = Annotated[
-    list[str],
-    Field(
-        default=[
-            "Artificial Intelligence",
-            "Machine Learning",
-            "Deep Learning",
-            "Neural Networks",
-            "NLP",
-            "Computer Vision",
-            "Data Science",
-            "Gemini",
-            "Bard",
-            "ChatGPT",
-            "GPT-4",
-            "DALL-E",
-            "MidJourney",
-            "Stable Diffusion",
-            "Claude",
-            "Whisper",
-        ],
-        description="List of keywords to filter AI news articles. This will be matched no matter the case",
-    ),
-]
-case_sen_search_kw = Annotated[
-    list[str],
-    Field(
-        default=[" AI ", "AI ", "AI ", "A.I.", " AI-", "AI-"],
-        description="List of keywords to filter AI News articles by. This will be an exact match",
-    ),
-]
-max_days_old = Annotated[
-    int,
-    Field(
-        default=2,
-        description="Maximum number of days since publication.",
-        ge=1,
-    ),
-]
-
-
 class AINewsConfig(BaseSettings):
     MIT_NEWS_FEED_URL: Annotated[
         str,
@@ -88,9 +46,6 @@ class AINewsConfig(BaseSettings):
             description="Name of the column that will be filtered by keywords",
         ),
     ]
-    CASE_INSEN_SEARCH_KW: case_insen_search_kw
-    CASE_SEN_SEARCH_KW: case_sen_search_kw
-    MAX_DAYS_OLD: max_days_old
 
     # To force to read .env file
     class Config:
