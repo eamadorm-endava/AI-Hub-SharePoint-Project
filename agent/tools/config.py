7b8ed040081b24d6b3a2354d39b80f188acc0e96
Emmanuel Amador (default) <emmanuel.amador@endava.com>
2025-10-20 22:22:17 +0000

    Add ImaGenToolConfig class, which will contain the configuration values for the image generation tool

--- agent/tools/config.py
+++ agent/tools/config.py
@@ -102,3 +102,47 @@ class TTSToolConfig(GCPToolConfig):
     @property
     def tool_name(self) -> str:
         return "text_to_speech"
+
+
+class ImaGenToolConfig(BaseSettings, validate_assignment=True):
+    """
+    Configuration class for Image Generation tool.
+    Inherits API key loading logic from GCPToolConfig.
+    """
+
+    MODEL_NAME: Annotated[
+        str,
+        Field(
+            default="gemini-2.5-flash-image",
+            description="Name of the model that will generate the images",
+        ),
+    ]
+    MODEL_TEMPERATURE: Annotated[
+        float,
+        Field(
+            default=0.8,
+            description="Controls randomness in model output: lower values make responses more focused, higher values more creative.",
+            ge=0,
+            le=1,
+        ),
+    ]
+    GCS_PATH: Annotated[
+        str,
+        Field(
+            default="images/",
+            description="Path inside the GCS Bucket where the images generated will be stored",
+        ),
+    ]
+    DEFAULT_GENERATED_IMAGES: Annotated[
+        int,
+        Field(
+            default=1,
+            description="Number of images to generate each time the tool is executed",
+            gt=0,
+            lt=5,
+        ),
+    ]
+
+    @property
+    def tool_name(self) -> str:
+        return "image_generation"
