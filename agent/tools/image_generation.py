da8283998f3da90159af48809fa83a04492a54c5
Emmanuel Amador (default) <emmanuel.amador@endava.com>
2025-10-21 02:06:25 +0000

    Creation of the tool to generate images

--- agent/tools/image_generation.py
+++ agent/tools/image_generation.py
@@ -3,6 +3,7 @@ from google import genai
 from google.genai import types
 import asyncio
 from agent.tools.config import ImaGenToolConfig
+from agent.tools.tool_schemas import ImaGenRequest, Image
 from utils.gcp.gcs import upload_bytes
 
 imagen_config = ImaGenToolConfig()
@@ -15,7 +16,7 @@ async def generate_image(
     general_image_name: str,
     llm_model: str = imagen_config.MODEL_NAME,
     gcs_path: str = imagen_config.GCS_PATH,
-) -> dict:
+) -> Image:
     """
     Calls a GenAI model exclusively for image generation based on text (prompt)
 
@@ -26,14 +27,14 @@ async def generate_image(
         gcs_path: str -> The path within the GCS bucket where the image should be stored (Ex: 'my_folder').
 
     Returns:
-        dict -> Dictionary with the image_name and the image_bytes
+        Image -> Image object containing the image data
     """
 
     logger.debug(f"{general_image_name = }")
     logger.debug(f"Input {prompt = }")
     logger.debug(f"{gcs_path = }")
 
-    image_data = {}
+    image_data = Image(prompt=prompt, image_name=general_image_name)
 
     # Code adapted from: https://googleapis.github.io/python-genai/#imagen
     # If you want async behaviour, just add call client.aio.<module to use>
@@ -45,83 +46,79 @@ async def generate_image(
         ),
     )
 
-    image_data["image_name"] = f"{gcs_path.strip('/')}/{general_image_name}.png"
     try:
-        image_data["image_bytes"] = response.generated_images[0].image.image_bytes
+        image_data.image_bytes = response.generated_images[0].image.image_bytes
+        image_data.gcs_path = f"{gcs_path.strip('/')}/{general_image_name}.png"
     except Exception as e:
-        logger.error(f"Eror while fetching the image: {e}")
-        return
+        error_message = f"Eror while fetching the image: {e}"
+        logger.error(error_message)
+        return error_message
 
     logger.debug("Image generated successfully")
     return image_data
 
 
-async def generate_images(prompts_info: list[dict]) -> list[str]:
+async def generate_images(image_requests: list[ImaGenRequest]) -> list[Image]:
     """
     Generates n number of images based on n number of requests
 
     Args:
-        requests: list[dict] -> List of dictionaries, each dictianary can be represented as one image generation request
-                                The dictionary must contain two keys:
-                                        - prompt: Prompt that will generate the image
-                                        - image_name: Name of the image
+        image_requests: list[ImaGenRequest] -> List of ImaGenRequest objects
 
     Returns:
-        list[str] -> A list of public urls where the images can be downloaded
+        list[Image] -> A list of Image objects, which contains the public URL to access the image generated
     """
     logger.info("Using Image Generation Tool...")
 
-    if not isinstance(prompts_info, list):
-        logger.error("prompts_info must be a list")
-        return
+    if not isinstance(image_requests, list):
+        error_message = "Parameter image_requests must be a list"
+        logger.error(error_message)
+        return error_message  # Agent could read this error and try to fix how this tool is used by him
+    elif len(image_requests) < 1:
+        error_message = "Parameter image_requests must have at least one entry"
+        logger.error(error_message)
+        return error_message
 
-    elif len(prompts_info) < 1:
-        logger.error("prompts_info must have at least one entry")
-        return
-    elif not all([isinstance(prompt_data, dict) for prompt_data in prompts_info]):
-        logger.error("Each entry of the prompts_info parameter must be a dictionary")
-        return
-    elif not all(
-        [
-            "prompt" in prompt_data.keys() and "image_name" in prompt_data.keys()
-            for prompt_data in prompts_info
-        ]
-    ):
-        logger.error("Each dictionary must contain the keys 'prompt' and 'image_name'")
-        return
+    logger.info(f"Generating {len(image_requests)} images...")
 
-    logger.info(f"Generating {len(prompts_info)} images...")
-
-    logger.debug(f"{prompts_info = }")
+    logger.debug(f"{image_requests = }")
     # Creating a list of generation tasks
     generation_tasks = list()
 
     logger.debug("Preparing generation requests")
-    for request in prompts_info:
+    for image_request in image_requests:
         task = generate_image(
-            prompt=request["prompt"], general_image_name=request["image_name"]
+            prompt=image_request.prompt, general_image_name=image_request.image_name
         )
         generation_tasks.append(task)
 
     logger.debug("Launching concurrent generation")
+
+    # images_data is a list of Image objects
     images_data = await asyncio.gather(*generation_tasks)
 
-    # As upload_image_from_memory is a syncronus function, it will be executed in
+    # As upload_bytes is a syncronus function, it will be executed in
     # different threads to reduce the execution time
     storage_tasks = list()
     for image_data in images_data:
+        # task is a function
         task = asyncio.to_thread(
             upload_bytes,
-            image_data["image_name"],  # First argument of the function - blob_name
+            image_data.gcs_path,  # First argument of the function - blob_name
             imagen_config._CLOUD_PROVIDER.BUCKET_NAME,  # Bucket name parameter
             imagen_config.CONTENT_TYPE,  # Content type parameter
-            image_data["image_bytes"],  # Bytes data parameter
+            image_data.image_bytes,  # Bytes data parameter
             True,  # last argument, make the gcs blob public, to return the image urls
         )
 
         storage_tasks.append(task)
 
+    # A list of what the task return, in this case, a list of image urls
     images_urls = await asyncio.gather(*storage_tasks, return_exceptions=True)
 
+    for image_index, image_data in enumerate(images_data):
+        image_data.url = images_urls[image_index]
+
     logger.info("Images Successfully Generated")
-    return images_urls
+
+    return images_data
