d131ea0f5b77fec87b2201fd2e6e294106314b65
Emmanuel Amador (default) <emmanuel.amador@endava.com>
2025-10-21 02:05:43 +0000

    Creation of an Image schema for the image_generation tool

--- agent/tools/tool_schemas.py
+++ agent/tools/tool_schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Annotated
 
 
-class ImagePromptInfo(BaseModel):
+class ImaGenRequest(BaseModel):
     prompt: Annotated[
         str,
         Field(description="Prompt describing the image to be generated"),
@@ -10,4 +10,29 @@ class ImagePromptInfo(BaseModel):
     image_name: Annotated[
         str,
         Field(description="Name of the image. (e.g. spider_in_the_rain)"),
+    ]
+
+
+class Image(ImaGenRequest):
+    gcs_path: Annotated[
+        str,
+        Field(
+            default=None,
+            description="Path in Google Cloud Storage where the image will be stored. (e.g.: my/gcs/folder/image_name.png)",
+            pattern=r"[\w]+/*\.png",
+        ),
+    ]
+    public_url: Annotated[
+        str,
+        Field(
+            default=None,  # In case the parameter is not set while creating the object instance
+            description="Public URL to access the image generated",
+        ),
+    ]
+    image_bytes: Annotated[
+        str,
+        Field(
+            default=None,
+            description="Bytes of the image generated",
+        ),
     ]
