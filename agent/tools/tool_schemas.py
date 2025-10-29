b5f25e97f8a293b68898214695e19937ab6e8b99
Emmanuel Amador (default) <emmanuel.amador@endava.com>
2025-10-29 01:42:11 +0000

    Add two schemas for the video generation: VideoGenerationRequest, and VideoGenResponse

--- agent/tools/tool_schemas.py
+++ agent/tools/tool_schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
-from typing import Annotated, Optional
+from pydantic import BaseModel, Field, HttpUrl
+from typing import Annotated, Optional, Literal
 
 
 class ImaGenRequest(BaseModel):
@@ -23,7 +23,7 @@ class Image(ImaGenRequest):
         ),
     ]
     public_url: Annotated[
-        str,
+        HttpUrl,
         Field(
             default=None,  # In case the parameter is not set while creating the object instance
             description="Public URL to access the image generated",
@@ -36,6 +36,54 @@ class Image(ImaGenRequest):
             description="Bytes of the image generated",
         ),
     ]
+
+    class Config:
+        validate_assignment = True
+
+
+class VideoGenRequest(BaseModel, validate_assignment=True):
+    """
+    Parameters of the Model that generates the video.
+
+    Check the documentation here: https://ai.google.dev/gemini-api/docs/video?example=dialogue
+    """
+
+    title: Annotated[
+        str,
+        Field(
+            description="Name of the title to be generated",
+            min_length=1,
+            pattern=r"^[\w/]+$",  # only upper and lower case, slashes, digits, and underscores
+        ),
+    ]
+    prompt: Annotated[
+        str,
+        Field(
+            description="Prompt describing the video",
+            min_length=1,
+        ),
+    ]
+    aspect_ratio: Annotated[
+        Literal["9:16", "16:9"],
+        Field(
+            default="9:16",
+            description="Aspect ratio of the video, only allowed 9:16 or 16:9",
+        ),
+    ]
+    duration_seconds: Annotated[
+        Literal[5, 6, 8],
+        Field(
+            default=8,
+            description="Duration of the video generated in seconds. Only 5, 6, or 8 are allowed",
+        ),
+    ]
+
+    class Config:
+        validate_assignment = True
+
+
+class VideoGenResponse(BaseModel, validate_assignment=True):
+    video_url: Annotated[HttpUrl, Field(description="public URL to access the video")]
 
     class Config:
         validate_assignment = True
