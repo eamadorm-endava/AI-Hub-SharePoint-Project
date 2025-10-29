2ed292257c5d728c6de4c9663d4ac5bc28a65eb5
Emmanuel Amador (default) <emmanuel.amador@endava.com>
2025-10-29 04:01:21 +0000

    Modify the instanciator of the tools module to now import the text_to_file tool from the audio module

--- agent/tools/__init__.py
+++ agent/tools/__init__.py
@@ -4,7 +4,7 @@ from .cloud_storage import (
     list_files_in_gcs_bucket,
 )
 from .image_generation import generate_images
-from .text_to_speech import text_to_speech
+from .audio import text_to_speech
 from .bigquery import query_news_table
 from .video_generation import generate_video
 
